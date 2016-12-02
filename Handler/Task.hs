9690df32a6578b6ce9fa6a1850a3dfbf5c84f377
Daniel Garcia <pel_daniel@hotmail.com>
2016-12-02 16:24:43 -0600

    Add show Task page

---
+++ Handler/Task.hs
@@ -0,0 +1,7 @@
+module Handler.Task where
+
+import Import
+
+getTaskR :: Handler String
+getTaskR =
+    return "Hello world!"
---
+++ test/Handler/TaskSpec.hs
@@ -0,0 +1,10 @@
+module Handler.TaskSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getTaskR" $ do
+        error "Spec not implemented: getTaskR"
+
