0cadf11ff449b8d73efb5f7d81186fe64196e8d2
Daniel Garcia <pel_daniel@hotmail.com>
2016-12-02 05:19:29 -0600

    Start project

---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "mvc" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
---
+++ test/Handler/HomeSpec.hs
@@ -0,0 +1,21 @@
+module Handler.HomeSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "Homepage" $ do
+        it "loads the index and checks it looks right" $ do
+          get HomeR
+          statusIs 200
+          htmlAnyContain "h1" "a modern framework for blazing fast websites"
+
+        -- This is a simple example of using a database access in a test.  The
+        -- test will succeed for a fresh scaffolded site with an empty database,
+        -- but will fail on an existing database with a non-empty user table.
+        -- it "leaves the user table empty" $ do
+        --   get HomeR
+        --   statusIs 200
+        --   users <- runDB $ selectList ([] :: [Filter User]) []
+        --   assertEq "user table empty" 0 $ length users
