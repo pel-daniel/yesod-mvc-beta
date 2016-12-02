1fca16679f0a892d5ccba53b1ba229cadeb3b07c
Daniel Garcia <pel_daniel@hotmail.com>
2016-12-02 16:13:40 -0600

    Remove setTitle from Home Handler

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -3,7 +3,5 @@ module Handler.Home where
 import Import
 
 getHomeR :: Handler Html
-getHomeR = do
-    defaultLayout $ do
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
+getHomeR =
+    defaultLayout $(widgetFile "homepage")
