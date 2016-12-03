c9ae1341cb3fdfcbce884171022e7c4efb6cd94f
Daniel Garcia <pel_daniel@hotmail.com>
2016-12-02 22:15:28 -0600

    Create Task

--- Handler/TaskNew.hs
+++ Handler/TaskNew.hs
@@ -10,8 +10,23 @@ taskForm =
         <$> areq textField "Description: " Nothing
         <*> areq boolField "Complete: " (Just False)
 
+
 getTaskNewR :: Handler Html
 getTaskNewR =
     do
         (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm taskForm
         defaultLayout $(widgetFile "tasks/new")
+
+
+postTaskNewR :: Handler Html
+postTaskNewR =
+    do
+        ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm taskForm
+        case res of
+            FormSuccess task ->
+                do
+                    taskId <- runDB $ insert task
+                    redirect $ TaskR taskId
+
+            _ ->
+                defaultLayout $(widgetFile "tasks/new")
